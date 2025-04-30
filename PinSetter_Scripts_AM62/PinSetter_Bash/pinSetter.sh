9ea610a2697af74dc1185c4ae25402fd61f1d4a2
Radai Ronald <ronald.radai@criterion.hu>
2025-04-30 12:52:54 +0200

    Set shorter waiting period, changed variable names to upper case

--- PinSetter_Scripts_AM62/PinSetter_Bash/pinSetter.sh
+++ PinSetter_Scripts_AM62/PinSetter_Bash/pinSetter.sh
@@ -4,38 +4,37 @@
 # to do meassurements on the AM62x platform.
 
 # Pin settings
-input_pin=1
-output_pin=2
+INPUT_PIN=1
+OUTPUT_PIN=2
 
 # Kill all task setting gpio pins
 pkill -f gpioset
 
 # Setting the output pin to its default value
-gpioset --consumer "pinSetter" --chip "/dev/gpiochip1" "$output_pin=0" &
+gpioset --consumer "pinSetter" --chip "/dev/gpiochip1" "$OUTPUT_PIN=0" &
 # Kill the task to be able to set the pin again
 pkill -f gpioset
-
 # Clear the screen to start the meassurements
 clear
-echo "Waiting for falling edge on GPIO pin $input_pin."
+echo "Waiting for falling edge on GPIO pin $INPUT_PIN."
 
 while true
 do
     # Waiting for a falling edge on the input pin
-    gpiomon --chip "/dev/gpiochip1" --consumer "pinSetter" --edge falling --debounce-period 0 --num-events 1 "$input_pin"
+    gpiomon --chip "/dev/gpiochip1" --consumer "pinSetter" --edge falling --debounce-period 0 --num-events 1 "$INPUT_PIN"
 
     # Setting the output pin to high
-    gpioset --consumer "pinSetter" --chip "/dev/gpiochip1" "$output_pin=1" &
+    gpioset --consumer "pinSetter" --chip "/dev/gpiochip1" "$OUTPUT_PIN=1" &
     # Wait for a little time to detect the rising edge
-    sleep 0.1
+    sleep 0.01
     # Kill the task to reset pin to 0
     pkill -f gpioset
 
     # Setting the output pin back to low
-    gpioset --consumer "pinSetter" --chip "/dev/gpiochip1" "$output_pin=0" &
+    gpioset --consumer "pinSetter" --chip "/dev/gpiochip1" "$OUTPUT_PIN=0" &
     # Kill the task to be able to set the pin again
     pkill -f gpioset
 
     # Printing out the result to the console
-    echo "Falling edge detected on GPIO pin $input_pin."
+    echo "Falling edge detected on GPIO pin $INPUT_PIN."
 done
