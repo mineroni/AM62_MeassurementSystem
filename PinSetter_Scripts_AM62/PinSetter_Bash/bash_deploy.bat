3b32d687e4fe86bbad5f48e73df1f198840a82df
Radai Ronald <ronald.radai@criterion.hu>
2025-04-30 12:53:28 +0200

    Made ip address changable in script

--- PinSetter_Scripts_AM62/PinSetter_Bash/bash_deploy.bat
+++ PinSetter_Scripts_AM62/PinSetter_Bash/bash_deploy.bat
@@ -1,11 +1,12 @@
 @echo off
-
-set ipAddr=10.155.74.12
-
 REM This script is used to deploy the PinSetter application on the AM62 platform.
+
+REM IP address of the target device
+set ipAddr=10.155.74.12
 
 REM Copy the pinSetter.sh script to the target device
 scp pinSetter.sh root@%ipAddr%:.
 
+REM Modify the script and set permissions and run it on the target device
 ssh root@%ipAddr% -t "sed -i 's/\r$//' pinSetter.sh; chmod +x pinSetter.sh; ./pinSetter.sh"
 
