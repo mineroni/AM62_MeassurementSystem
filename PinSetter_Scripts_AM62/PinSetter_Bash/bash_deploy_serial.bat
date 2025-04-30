63de4f90bf936bdda5f810fb5f1944fd2512fac9
Radai Ronald <ronald.radai@criterion.hu>
2025-04-30 13:14:05 +0200

    Remove unnecessary  comment

--- PinSetter_Scripts_AM62/PinSetter_Bash/bash_deploy_serial.bat
+++ PinSetter_Scripts_AM62/PinSetter_Bash/bash_deploy_serial.bat
@@ -4,8 +4,6 @@ REM This script is used to deploy the PinSetter application on the AM62 platform
 REM IP address of the target device
 set ipAddr=10.155.74.12
 
-REM This script is used to deploy the PinSetter application on the AM62 platform.
-
 REM Copy the pinSetter.sh script to the target device
 scp pinSetter_serial.sh root@%ipAddr%:bashTest/
 
